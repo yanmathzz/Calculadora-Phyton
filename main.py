158f7a5ad0253c489d7002014174f4b4fe283dbb
yan matheus <ymatheus706@gmail.com>
2023-03-15 21:56:36 -0300

    criando botões

--- main.py
+++ main.py
@@ -16,8 +16,22 @@ janela.title("Calculadora")
 janela.geometry("235x318")
 janela.config(bg=cor1)
 
-frame_tela = Frame(janela, width=235, height=50)
+
+#criando frames
+frame_tela = Frame(janela, width=235, height=50, bg=cor3)
 frame_tela.grid(row=0, column=0)
+
+frame_corpo = Frame(janela, width=235, height=268)
+frame_corpo.grid(row=1, column=0)
+
+#criando botoes 
+
+b_1 = Button(frame_corpo, text="C", width=11, height=2)
+b_1.place(x=0, y=0)
+b_1 = Button(frame_corpo, text="%", width=11, height=2)
+b_1.place(x=100, y=0)
+b_1 = Button(frame_corpo, text="/", width=11, height=2)
+b_1.place(x=177, y=0)
 
 
 janela.mainloop()
