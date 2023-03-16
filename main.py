c7a7108be82445d4f3761742fbfcd11ea6004f10
yan matheus <ymatheus706@gmail.com>
2023-03-15 21:09:39 -0300

    cores

--- main.py
+++ main.py
@@ -2,7 +2,22 @@
 from tkinter import *
 from tkinter import ttk
 
+#cores
+
+cor1 = "#3b3b3b" #preto/black
+cor2 = "#feffff" #branco/white 
+cor3 = "#38576b" #azul carregado
+cor4 = "#ECEFF1" #cinzento
+cor5 = "#FFAB40" #laranja/Orange
+
+
 janela =Tk()
 janela.title("Calculadora")
+janela.geometry("235x318")
+janela.config(bg=cor1)
+
+frame_tela = Frame(janela, width=235, height=50)
+frame_tela.grid(row=0, column=0)
+
 
 janela.mainloop()
