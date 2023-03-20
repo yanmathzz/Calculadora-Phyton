f7e489532f626a821e4e7a353b68166bb6276734
yan matheus <ymatheus706@gmail.com>
2023-03-20 18:50:05 -0300

    valores

--- main.py
+++ main.py
@@ -25,13 +25,23 @@ frame_corpo = Frame(janela, width=235, height=268)
 frame_corpo.grid(row=1, column=0)
 
 
+# variável todos valores
+
+todos_valores = ''
+
+
 # criando Função
 
-def entrar_valores():
-    resultado = eval('9/9')
+def entrar_valores(event):
 
+    global todos_valores
+
+    todos_valores = todos_valores + str(event)
+    
+    resultado = eval('9/9')
+    
     # passando valor para a tela
-    valor_texto.set(resultado)
+    valor_texto.set(todos_valores)
 
 
 
@@ -50,7 +60,7 @@ app_label.place(x=0, y=0)
 b_1 = Button(frame_corpo, text="C", width=11, height=2, bg=cor4,
              font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_1.place(x=0, y=0)
-b_2 = Button(frame_corpo, command=valor_texto, text="%", width=5, height=2, bg=cor4,
+b_2 = Button(frame_corpo, command = lambda: entrar_valores("%"), text="%", width=5, height=2, bg=cor4,
              font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_2.place(x=118, y=0)
 b_3 = Button(frame_corpo, text="/", width=5, height=2, bg=cor5,
