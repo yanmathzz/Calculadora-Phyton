6e5f83a662a88891a96d72688d9cc7b93d14081f
yan matheus <ymatheus706@gmail.com>
2023-03-20 18:40:33 -0300

    valor_texto

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ app_label.place(x=0, y=0)
 b_1 = Button(frame_corpo, text="C", width=11, height=2, bg=cor4,
              font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_1.place(x=0, y=0)
-b_2 = Button(frame_corpo, text="%", width=5, height=2, bg=cor4,
+b_2 = Button(frame_corpo, command=valor_texto, text="%", width=5, height=2, bg=cor4,
              font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_2.place(x=118, y=0)
 b_3 = Button(frame_corpo, text="/", width=5, height=2, bg=cor5,
