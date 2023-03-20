46abb47d330c4d4f104474a8fdb2fe570ed2ff98
yan matheus <ymatheus706@gmail.com>
2023-03-20 19:31:02 -0300

    edição

--- main.py
+++ main.py
@@ -29,8 +29,10 @@ frame_corpo.grid(row=1, column=0)
 
 todos_valores = ''
 
+
 # criando Label
 valor_texto = StringVar()
+
 
 # criando Função
 
@@ -39,11 +41,10 @@ def entrar_valores(event):
     global todos_valores
 
     todos_valores = todos_valores + str(event)
-    
+
     # passando valor para a tela
 
     valor_texto.set(todos_valores)
-
 
 
 # função para calcular
@@ -51,18 +52,16 @@ def entrar_valores(event):
 def calcular():
     global todos_valores
     resultado = eval(todos_valores)
-    
+
     valor_texto.set(str(resultado))
 
-# função limpar tela 
+
+# função limpar tela
 
 def limpar_tela():
     global todos_valores
     todos_valores = ''
     valor_texto.set('')
-
-
-
 
 
 # criando Label
@@ -76,65 +75,64 @@ app_label.place(x=0, y=0)
 
 # criando botoes
 
-b_1 = Button(frame_corpo, command= limpar_tela, text="C", width=11, height=2, bg=cor4,
+b_1 = Button(frame_corpo, command=limpar_tela, text="C", width=11, height=2, bg=cor4,
              font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_1.place(x=0, y=0)
-b_2 = Button(frame_corpo, command = lambda: entrar_valores("%"), text="%", width=5, height=2, bg=cor4,
+b_2 = Button(frame_corpo, command=lambda: entrar_valores("%"), text="%", width=5, height=2, bg=cor4,
              font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_2.place(x=118, y=0)
-b_3 = Button(frame_corpo, command = lambda: entrar_valores("/"), text="/", width=5, height=2, bg=cor5,
+b_3 = Button(frame_corpo, command=lambda: entrar_valores("/"), text="/", width=5, height=2, bg=cor5,
              fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_3.place(x=177, y=0)
 
-b_4 = Button(frame_corpo, command = lambda: entrar_valores("7"), text="7", width=5, height=2, bg=cor4,
+b_4 = Button(frame_corpo, command=lambda: entrar_valores("7"), text="7", width=5, height=2, bg=cor4,
              font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_4.place(x=0, y=52)
-b_4 = Button(frame_corpo, command = lambda: entrar_valores("8"), text="8", width=5, height=2, bg=cor4,
+b_4 = Button(frame_corpo, command=lambda: entrar_valores("8"), text="8", width=5, height=2, bg=cor4,
              font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_4.place(x=59, y=52)
-b_4 = Button(frame_corpo, command = lambda: entrar_valores("9"), text="9", width=5, height=2, bg=cor4,
+b_4 = Button(frame_corpo, command=lambda: entrar_valores("9"), text="9", width=5, height=2, bg=cor4,
              font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_4.place(x=118, y=52)
-b_3 = Button(frame_corpo, command = lambda: entrar_valores("*"), text="*", width=5, height=2, bg=cor5,
+b_3 = Button(frame_corpo, command=lambda: entrar_valores("*"), text="*", width=5, height=2, bg=cor5,
              fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_3.place(x=177, y=52)
 
-b_4 = Button(frame_corpo, command = lambda: entrar_valores("4"), text="4", width=5, height=2, bg=cor4,
+b_4 = Button(frame_corpo, command=lambda: entrar_valores("4"), text="4", width=5, height=2, bg=cor4,
              font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_4.place(x=0, y=104)
-b_5 = Button(frame_corpo, command = lambda: entrar_valores("5"), text="5", width=5, height=2, bg=cor4,
+b_5 = Button(frame_corpo, command=lambda: entrar_valores("5"), text="5", width=5, height=2, bg=cor4,
              font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_5.place(x=59, y=104)
-b_6 = Button(frame_corpo, command = lambda: entrar_valores("6"), text="6", width=5, height=2, bg=cor4,
+b_6 = Button(frame_corpo, command=lambda: entrar_valores("6"), text="6", width=5, height=2, bg=cor4,
              font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_6.place(x=118, y=104)
-b_7 = Button(frame_corpo, command = lambda: entrar_valores("-"), text="-", width=5, height=2, bg=cor5,
+b_7 = Button(frame_corpo, command=lambda: entrar_valores("-"), text="-", width=5, height=2, bg=cor5,
              fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_7.place(x=177, y=104)
 
-b_8 = Button(frame_corpo, command = lambda: entrar_valores("1"), text="1", width=5, height=2, bg=cor4,
+b_8 = Button(frame_corpo, command=lambda: entrar_valores("1"), text="1", width=5, height=2, bg=cor4,
              font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_8.place(x=0, y=156)
-b_9 = Button(frame_corpo, command = lambda: entrar_valores("2"), text="2", width=5, height=2, bg=cor4,
+b_9 = Button(frame_corpo, command=lambda: entrar_valores("2"), text="2", width=5, height=2, bg=cor4,
              font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_9.place(x=59, y=156)
-b_10 = Button(frame_corpo, command = lambda: entrar_valores("3"), text="3", width=5, height=2, bg=cor4,
+b_10 = Button(frame_corpo, command=lambda: entrar_valores("3"), text="3", width=5, height=2, bg=cor4,
               font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_10.place(x=118, y=156)
-b_11 = Button(frame_corpo, command = lambda: entrar_valores("+"), text="+", width=5, height=2, bg=cor5,
+b_11 = Button(frame_corpo, command=lambda: entrar_valores("+"), text="+", width=5, height=2, bg=cor5,
               fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_11.place(x=177, y=156)
 
-b_12 = Button(frame_corpo, command = lambda: entrar_valores("0"), text="0", width=11, height=2, bg=cor4,
+b_12 = Button(frame_corpo, command=lambda: entrar_valores("0"), text="0", width=11, height=2, bg=cor4,
               font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_12.place(x=0, y=208)
-b_13 = Button(frame_corpo, command = lambda: entrar_valores("."), text=".", width=5, height=2, bg=cor4,
+b_13 = Button(frame_corpo, command=lambda: entrar_valores("."), text=".", width=5, height=2, bg=cor4,
               font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_13.place(x=118, y=208)
-b_14 = Button(frame_corpo, command = calcular, text="=", width=5, height=2, bg=cor5,
+b_14 = Button(frame_corpo, command=calcular, text="=", width=5, height=2, bg=cor5,
               fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_14.place(x=177, y=208)
-
 
 
 janela.mainloop()
